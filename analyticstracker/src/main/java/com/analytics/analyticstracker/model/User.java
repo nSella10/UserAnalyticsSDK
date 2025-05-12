b2d3fb4bea08670cf7e0f0fe0f1e2db64083eb06
noam sella <noamsella10@gmail.com>
2025-05-12 17:34:27 +0300

    auth_activity

---
+++ analyticstracker/src/main/java/com/analytics/analyticstracker/model/User.java
@@ -0,0 +1,77 @@
+package com.analytics.analyticstracker.model;
+
+
+//user model in SDK
+public class User {
+
+    public enum Gender {
+        MALE, FEMALE, OTHER
+    }
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private int age;
+    private Gender gender;
+
+    // Constructor (send data to backend)
+    public User(String firstName, String lastName,int age, Gender gender,String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+        this.password = password;
+    }
+
+    //getter & setter
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
